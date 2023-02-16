c7592fc841baef95daf6dcbe88c0b9cd7c1080fe
archit-bhatnagar <architaspiring@gmail.com>
2023-02-16 13:57:23 +0800

    Hash inconsistent(maybe error with reference or how we input to the code

---
+++ src/main.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "hash.h"
+// #define ASCON_PRINT_STATE
+
+void main(){
+    unsigned char* out=malloc(33*sizeof(*out));
+    const unsigned char* in="00010203";
+    // unsigned long long len=2;
+    unsigned long long len= (unsigned long long)strlen(in);
+    // printf("the LENGTH is %llu \n",len);
+
+    int finish= crypto_hash(out,in,len);
+    
+    out[32]=NULL; // to get the string in a printable form
+
+    printf("the output string is \n");
+    for (int i = 0; i < 32; i++)
+    {
+        printf("%02X ",out[i]);
+    }
+    
+    printf("\n");
+    free(out);
+
+}
